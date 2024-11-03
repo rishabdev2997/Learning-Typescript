c8f1a020cd112bff9dad8acc72d2e4fc2d8e942d
rishab <rishabstudent2000@gmail.com>
2024-11-04 00:29:17 +0530

    done

---
+++ src/a.ts
@@ -0,0 +1,64 @@
+// function sum(a : number, b : number): number {
+//     return a + b;
+// }
+
+// const value = sum(1,2);
+
+// console.log(value);
+
+// function isLegal(age: number):boolean{
+//     if(age > 18 ){
+//         return true;
+//     } else {
+//         return false
+//     }
+// }
+
+// let x = isLegal(19);
+// console.log(x);
+
+// function runAfter1S(fn: () => void){
+//     setTimeout(fn,1000);
+// }
+
+// runAfter1S(function(){
+//     console.log("hi there");
+// })
+
+// interface User{
+//     firstName: string,
+//     lastName: string,
+//     age: number
+// }
+// function isLegal1(user:User){
+//     if(user.age > 18 ){
+//         return true;
+//     } else {
+//         return false
+//     }
+// }
+// function greet2(user:User){
+//     return ("hi There " + user.firstName);
+// }
+
+
+
+// let y = isLegal1({
+//     firstName: "John",
+//     lastName: "tudu",
+//     age : 20
+// })
+// let z = greet2({
+//     firstName: "John",
+//     lastName: "tudu",
+//     age : 20
+// })
+
+// console.log(y);
+
+// console.log(z);
+
+function doSomething(keyPressed: string){
+    // do something
+}
+
