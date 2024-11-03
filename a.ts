d94a1eabaefdde1051cf9a371b9da49b0c86ddaf
rishab <rishabstudent2000@gmail.com>
2024-11-03 16:26:19 +0530

    isLegal

--- a.ts
+++ a.ts
@@ -1,2 +1,18 @@
-const x: number = 1;
+function sum(a : number, b : number): number {
+    return a + b;
+}
+
+const value = sum(1,2);
+
+console.log(value);
+
+function isLeagal(age: number):boolean{
+    if(age > 18 ){
+        return true;
+    } else {
+        return false
+    }
+}
+
+let x = isLeagal(19);
 console.log(x);
